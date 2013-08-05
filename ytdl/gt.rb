1a69611340d90107ac492abab21b6a27565cd291
carusocr <carusocr@ldc.upenn.edu>
2013-08-05 14:40:40 -0400

    finished metadata processing

--- ytdl/gt.rb
+++ ytdl/gt.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 # Script to download youtube videos, including all videos from a user's channel. 
+# needed additions: Log file.
 
 require 'mysql'
 require 'nokogiri'
@@ -52,13 +53,13 @@ def build_subscription_clips
 
 end
 
-def build_download_list
-	ytq = $m.query("select id, url from vscout_url where url like '%youtu%' and media_file is NULL and date_found like '#{$daterange}%'")
+def build_downloads
+	ytq = $m.query("select id, url from vscout_url where url like '%youtu%' and (media_file is NULL or media_file = 'fail') and date_found like '#{$daterange}%'")
 	ytq.each_hash do |ytc|
 		$download_urls["#{ytc['url'][/watch\?v=(.{11})/,1]}"] = ytc['id']
 	end
 	$download_urls.sort_by.each do |url, id|
-#		puts "#{url} #{id}\n"
+		puts "Downloading #{url} #{id}\n"
 		download_clip(url,id)
 	end
 
@@ -68,41 +69,42 @@ def download_clip(url,id)
 
 	video_clip = "VVC" + format("%06d",id) + ".mp4"
 	puts "Download command is youtube-dl -w -f 18 -o #{DATADIR}/#{video_clip} #{url}\n"
+	# download if file exists, go straight to metadata otherwise?
 	#`youtube-dl -w -f 18 -o #{ofil} #{url}`
 	if File.exist?("#{DATADIR}/#{video_clip}")
 		puts "#{url} successfully downloaded as #{video_clip}\n"
-		generate_metadata(video_clip)
-	#add rescue for fail here
+		generate_metadata(video_clip,id)
+	else 
+		puts "Download failed!\n"
+		$m.query("update vscout_url set media_file = 'fail' where id = #{id}")
 	end
-	#add filecheck, error handling, metadata here
 
 end
 
-def generate_metadata(video_clip)
-	info = `mp4info #{DATADIR}/#{video_clip}`.split("\n")
-	codec_video = info[3][/video(.+), \d+\.\d+ secs,/,1].strip!
-	codec_audio = info[4][/audio(.+), \d+\.\d+ secs,/,1].strip!
-	duration = info[4][/, (\d+\.\d+) secs,/,1].to_f.round
-	puts "Clip #{video_clip} duration #{duration} codec #{codec_video}/#{codec_audio}\n"
-=begin
-	check file type
-	get codec and duration info
-	update database
-	handle errors gracefully
-=end
+def generate_metadata(video_clip,id)
+
+	begin
+		info = `mp4info #{DATADIR}/#{video_clip}`.split("\n")
+		codec_video = info[3][/video(.+), \d+\.\d+ secs,/,1].strip!
+		codec_audio = info[4][/audio(.+), \d+\.\d+ secs,/,1].strip!
+		duration = info[4][/, (\d+\.\d+) secs,/,1].to_f.round
+		$m.query("update vscout_url set codec = '#{codec_video}/#{codec_audio}', duration = #{duration}, media_file = '#{video_clip}' where id = #{id}")
+	rescue
+		$m.query("update vscout_url set codec = 'fail', duration = 'fail', media_file = 'fail' where id = #{id}")
+	end
+
 end
 
 def add_child_clip_to_database(clip_url, id)
+
 	begin
 		$m.query("insert into vscout_url (url,url_md5, parent_url, date_found) values ('http://youtube.com/watch?v=#{clip_url}',md5('#{clip_url}'),#{id},current_timestamp)")
-#puts "insert into vscout_url (url, parent_url, date_found) values ('http://youtube.com/watch?v=#{clip_url}',#{id},current_timestamp)\n"
 	rescue Mysql::Error
 	end
+
 end
 
 build_parent_clips
 build_subscription_clips
-build_download_list
-#generate_metadata
-# each loop down here using array made with build_download_list, calling download_clip and then update database
-#need to add some better dupe checks...maybe when building children clips, compare against existing clips to make sure they're not already in there?
+build_downloads
+$m.close
