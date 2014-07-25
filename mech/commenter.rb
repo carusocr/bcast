e78eb62d59f15e79f367ec6a58c67f8ce571adc8
Chris Caruso <carusocr@ldc.upenn.edu>
2014-07-25 14:08:51 -0400

    added notes on simupost method

--- mech/commenter.rb
+++ mech/commenter.rb
@@ -96,4 +96,30 @@ Selenium::WebDriver::Error::ElementNotVisibleError: Element is not currently vis
 from [remote server] file:///var/folders/nD/nDNsuhVXFiyh2wjEgJ0eXk+++TI/-Tmp-/webdriver-profile20140708-54519-10ezhca/extensions/fxdriver@googlecode.com/components/command_processor.js:8225:5:in `fxdriver.preconditions.visible'
 page.execute_script("$('input[name=\"number\"]').focus()")
 
+Method to simulate post?
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 =end
