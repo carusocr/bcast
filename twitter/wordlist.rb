436d364baf320ad58bccc23cc34c06f11053932e
carusocr <carusocr@ldc.upenn.edu>
2014-08-07 11:07:38 -0400

    ...

--- twitter/wordlist.rb
+++ twitter/wordlist.rb
@@ -4,33 +4,33 @@
 # Date Created: 18 July 2014
 # script creates a Twitter client, assembles array of most common words,
 # tracks tweets matching words
+# NOT unique IDs, need to postprocess
 
 # list of elements returned in status
 # https://dev.twitter.com/docs/platform-objects/tweets
-# b = a.join(',').gsub("\n",'')
-# a = File.readlines('uzbek.txt')
 
 require 'yaml'
 require 'tweetstream'
 require 'json'
 cfgfile = 'auth.cfg'
-datadir = '/media/181cf896-8dc3-40f6-8fcc-6b3513d78153/tweets'
-wordlist = 'uzbek.txt'
+abort "Enter collection type!" unless ARGV[0]
+collection = ARGV[0]
+wordlist = collection + ".txt"
 
 cnf = YAML::load(File.open(cfgfile))
+datadir = cnf[collection]['datadir']
+ofil = `date +%Y%m%d_%H%M`.chop + "_#{collection}.txt"
 
 TweetStream.configure do |config|
-  config.consumer_key       = cnf['sample']['con_key']
-  config.consumer_secret    = cnf['sample']['con_sec']
-  config.oauth_token        = cnf['sample']['o_tok']
-  config.oauth_token_secret = cnf['sample']['o_tok_sec']
-  config.auth_method        = cnf['sample']['a_meth']
+  config.consumer_key       = cnf[collection]['con_key']
+  config.consumer_secret    = cnf[collection]['con_sec']
+  config.oauth_token        = cnf[collection]['o_tok']
+  config.oauth_token_secret = cnf[collection]['o_tok_sec']
+  config.auth_method        = cnf[collection]['a_meth']
 end
 
-ofil = `date +%Y%m%d_%k%M`.chop + '_uzbek.txt'
-#ofil = `date +%Y%m%d_%k%M`.chop + '_RT.txt'
-searchterm = File.readlines('uzbek.txt').join(',').gsub("\n","")
-#searchterm = 'RT,the'
+searchterm = File.readlines("#{wordlist}").join(',').gsub("\n","")
+#searchterm = 'RT'
 
 tweetfile = File.open("#{datadir}/#{ofil}",'a')
 
